879c658cda0bfb39165393b88221f7fb899c2fcc
Patryk Małek <pmalek@sumologic.com>
2020-10-07 13:50:13 +0200

    Add docker image tests to build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -2,55 +2,88 @@
 
 VERSION="${TRAVIS_TAG:-0.0.0}"
 VERSION="${VERSION#v}"
-: "${DOCKER_TAG:=sumologic/kubernetes-tools}"
+: "${DOCKER_IMAGE:=sumologic/kubernetes-tools}"
 : "${DOCKER_USERNAME:=sumodocker}"
-DOCKER_TAGS="https://registry.hub.docker.com/v1/repositories/sumologic/kubernetes-tools/tags"
 
-echo "Starting build process in: $(pwd) with version tag: ${VERSION}"
 err_report() {
     echo "Script error on line $1"
     exit 1
 }
 trap 'err_report $LINENO' ERR
 
-# Set up Github
-if [ -n "$GITHUB_TOKEN" ]; then
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis CI"
-  git remote add origin-repo https://${GITHUB_TOKEN}@github.com/SumoLogic/sumologic-kubernetes-tools.git > /dev/null 2>&1
-  git fetch --unshallow origin-repo
-  git checkout $TRAVIS_PULL_REQUEST_BRANCH
-fi
-
-echo "Building docker image with $DOCKER_TAG:local in $(pwd)..."
-docker build . -f deploy/docker/Dockerfile -t $DOCKER_TAG:local --no-cache
+# Functions
 
 function push_docker_image() {
   local version="$1"
 
-  echo "Tagging docker image $DOCKER_TAG:local with $DOCKER_TAG:$version..."
-  docker tag $DOCKER_TAG:local $DOCKER_TAG:$version
-  echo "Pushing docker image $DOCKER_TAG:$version..."
-  echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-  docker push $DOCKER_TAG:$version
+  echo "Tagging docker image ${DOCKER_IMAGE}:local with ${DOCKER_IMAGE}:${version}..."
+  docker tag "${DOCKER_IMAGE}:local" "${DOCKER_IMAGE}:${version}"
+  echo "Pushing docker image ${DOCKER_IMAGE}:${version}..."
+  echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
+  docker push "${DOCKER_IMAGE}:${version}"
 }
 
-if [ -n "$DOCKER_PASSWORD" ] && [ -n "$TRAVIS_TAG" ]; then
-  push_docker_image "$VERSION"
-
-  # if the tag is a GA and not a prerelease then retag the latest to it
-  if [[ "$TRAVIS_TAG" =~ v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-	  push_docker_image "latest"
+function set_up_github() {
+  if [[ -z "${GITHUB_TOKEN}" ]]; then
+    echo "GITHUB_TOKEN not provided - skipping git setup"
+    return
   fi
 
-elif [ -n "$DOCKER_PASSWORD" ] && [[ "$TRAVIS_BRANCH" == "master" || "$TRAVIS_BRANCH" =~ ^release-v[0-9]+\.[0-9]+$ ]] && [ "$TRAVIS_EVENT_TYPE" == "push" ]; then
-  dev_build_tag=$(git describe --tags --always)
-  dev_build_tag=${dev_build_tag#v}
-  push_docker_image "$dev_build_tag"
-  push_docker_image "$TRAVIS_BRANCH"
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+  git remote add origin-repo "https://${GITHUB_TOKEN}@github.com/SumoLogic/sumologic-kubernetes-tools.git" > /dev/null 2>&1
+  git fetch --unshallow origin-repo
+  git checkout "${TRAVIS_PULL_REQUEST_BRANCH}"
+}
 
-else
-  echo "Skip Docker pushing"
-fi
+function build_docker_image() {
+  echo "Building docker image with ${DOCKER_IMAGE}:local in $(pwd)..."
+  docker build . -f deploy/docker/Dockerfile -t "${DOCKER_IMAGE}:local" --no-cache
+}
 
-echo "DONE"
+function test_docker_image() {
+  local apps="stress-tester k8s-api-test receiver-mock check fix-log-symlinks tools-usage template template-dependency"
+
+  echo
+  echo "Running docker image tests..."
+
+  for app in ${apps}; do
+    echo "Testing ${app}..."
+    docker run --rm "${DOCKER_IMAGE}:local" "${app}" --help >/dev/null
+  done
+
+  echo "Testing docker image's CMD..."
+  docker run --rm "${DOCKER_IMAGE}:local" >/dev/null
+
+  echo "Docker image tests OK"
+}
+
+function push_docker_images() {
+  if [[ -z "${DOCKER_PASSWORD}" ]]; then
+    echo "Skip Docker image push (DOCKER_PASSWORD was not provided)"
+    return
+  fi
+
+  if [[ -n "${TRAVIS_TAG}" ]]; then
+    push_docker_image "${VERSION}"
+
+    # if the tag is a GA and not a prerelease then retag the latest to it
+    if [[ "${TRAVIS_TAG}" =~ v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+      push_docker_image "latest"
+    fi
+  elif [[ "${TRAVIS_BRANCH}" == "master" || "${TRAVIS_BRANCH}" =~ ^release-v[0-9]+\.[0-9]+$ ]] && [[ "${TRAVIS_EVENT_TYPE}" == "push" ]]; then
+    dev_build_tag="$(git describe --tags --always)"
+    dev_build_tag="${dev_build_tag#v}"
+    push_docker_image "${dev_build_tag}"
+    push_docker_image "${TRAVIS_BRANCH}"
+  fi
+}
+
+# Main
+
+echo "Starting build process in '$(pwd)' with version tag: ${VERSION}"
+set_up_github
+build_docker_image
+test_docker_image
+push_docker_images
+echo "Done with build.sh"
