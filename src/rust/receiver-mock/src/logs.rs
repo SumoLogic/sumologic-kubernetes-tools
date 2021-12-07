2849c0234f6e9fb0fc974ca9e60aa337276384f9
Mikolaj Swiatek <mswiatek@sumologic.com>
2021-12-07 18:20:43 +0100

    feat(receiver-mock): add X-Sumo-Fields parsing for logs

--- src/rust/receiver-mock/src/logs.rs
+++ src/rust/receiver-mock/src/logs.rs
@@ -115,6 +115,28 @@ fn get_timestamp_from_body(body: &str) -> Option<u64> {
     return timestamp.as_u64();
 }
 
+/// Parse the value of the X-Sumo-Fields header into a map of field name to field value
+fn parse_sumo_fields_header_value(header_value: &str) -> Result<HashMap<String, String>, anyhow::Error> {
+    let mut field_values = HashMap::new();
+    if header_value.trim().len() == 0 {
+        return Ok(field_values);
+    }
+    for entry in header_value.split(",") {
+        match entry.trim().split_once("=") {
+            Some((field_name, field_value)) => {
+                field_values.insert(field_name.to_string(), field_value.to_string())
+            }
+            None => {
+                return Err(anyhow!(
+                    "Failed to parse X-Sumo-Fields, no `=` in {}",
+                    entry
+                ))
+            }
+        };
+    }
+    return Ok(field_values);
+}
+
 #[cfg(test)]
 mod tests {
     // Note this useful idiom: importing names from outer (for mod tests) scope.
@@ -191,5 +213,47 @@ mod tests {
         assert!(get_timestamp_from_body(r#"{"timestamp": 1.5}"#).is_none());
         assert!(get_timestamp_from_body(r#"{"log": "Some log message"}"#).is_none());
         assert!(get_timestamp_from_body("Not json at all").is_none())
+    }
+
+    #[test]
+    fn test_parse_sumo_fields_valid() {
+        let single_pair = "_collector=test";
+        assert_eq!(
+            parse_sumo_fields_header_value(single_pair).unwrap(),
+            HashMap::from([(String::from("_collector"), String::from("test"))])
+        );
+
+        let multiple_pairs = "service=collection-kube-state-metrics, deployment=collection-kube-state-metrics, node=sumologic-control-plane";
+        assert_eq!(
+            parse_sumo_fields_header_value(multiple_pairs).unwrap(),
+            HashMap::from([
+                (
+                    String::from("service"),
+                    String::from("collection-kube-state-metrics")
+                ),
+                (
+                    String::from("deployment"),
+                    String::from("collection-kube-state-metrics")
+                ),
+                (
+                    String::from("node"),
+                    String::from("sumologic-control-plane")
+                )
+            ])
+        );
+
+        let empty = "";
+        assert_eq!(
+            parse_sumo_fields_header_value(empty).unwrap(),
+            HashMap::new()
+        );
+    }
+
+    #[test]
+    fn test_parse_sumo_fields_invalid() {
+        let invalid_inputs = [",", "no_equals"];
+        for input in invalid_inputs {
+            assert!(parse_sumo_fields_header_value(input).is_err())
+        }
     }
 }
