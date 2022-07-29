cf5ddc8fb3b66457d1b94f64001df8016bc1806b
Dominik Rosiek <drosiek@sumologic.com>
2022-07-29 15:48:11 +0200

    feat(update-collection-v3): add kube-state-metrics migration
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>

--- src/go/cmd/update-collection-v3/migrate.go
+++ src/go/cmd/update-collection-v3/migrate.go
@@ -2,5 +2,78 @@ package main
 
 // TODO: implement
 func migrate(valuesV2 *ValuesV2) (ValuesV3, error) {
-	return ValuesV3(*valuesV2), nil
+	valuesV3 := ValuesV3{
+		Rest:                valuesV2.Rest,
+		KubePrometheusStack: migrateKubePrometheusStack(valuesV2.KubePrometheusStack),
+	}
+
+	return valuesV3, nil
+}
+
+func migrateKubePrometheusStack(valuesV2 *KubePrometheusStackV2) *KubePrometheusStackV3 {
+	if valuesV2 == nil {
+		return nil
+	}
+
+	return &KubePrometheusStackV3{
+		Rest:             valuesV2.Rest,
+		KubeStateMetrics: migrateKubeStateMetrics(valuesV2.KubeStateMetrics),
+	}
+}
+
+func migrateKubeStateMetrics(valuesV2 *KubeStateMetricsV2) *KubeStateMetricsV3 {
+	if valuesV2 == nil {
+		return nil
+	}
+
+	return &KubeStateMetricsV3{
+		Rest:       valuesV2.Rest,
+		Collectors: migrateKubeStateMetricsCollectors(valuesV2.Collectors),
+	}
+}
+
+func migrateKubeStateMetricsCollectors(collectors *map[string]bool) *[]string {
+	if collectors == nil {
+		return nil
+	}
+
+	kubeStateMetricsCollectorsList := []string{
+		"certificatesigningrequests",
+		"configmaps",
+		"cronjobs",
+		"daemonsets",
+		"deployments",
+		"endpoints",
+		"horizontalpodautoscalers",
+		"ingresses",
+		"jobs",
+		"limitranges",
+		"mutatingwebhookconfigurations",
+		"namespaces",
+		"networkpolicies",
+		"nodes",
+		"persistentvolumeclaims",
+		"persistentvolumes",
+		"poddisruptionbudgets",
+		"pods",
+		"replicasets",
+		"replicationcontrollers",
+		"resourcequotas",
+		"secrets",
+		"services",
+		"statefulsets",
+		"storageclasses",
+		"validatingwebhookconfigurations",
+		"volumeattachments",
+	}
+
+	returnList := []string{}
+
+	for _, key := range kubeStateMetricsCollectorsList {
+		if value, ok := (*collectors)[key]; ok && !value {
+			continue
+		}
+		returnList = append(returnList, key)
+	}
+	return &returnList
 }
--- src/go/cmd/update-collection-v3/valuesv3.go
+++ src/go/cmd/update-collection-v3/valuesv3.go
@@ -1,5 +1,16 @@
 package main
 
 type ValuesV3 struct {
-	Rest map[string]interface{} `yaml:",inline"`
+	KubePrometheusStack *KubePrometheusStackV3 `yaml:"kube-prometheus-stack"`
+	Rest                map[string]interface{} `yaml:",inline"`
+}
+
+type KubeStateMetricsV3 struct {
+	Collectors *[]string              `yaml:"collectors"`
+	Rest       map[string]interface{} `yaml:",inline"`
+}
+
+type KubePrometheusStackV3 struct {
+	KubeStateMetrics *KubeStateMetricsV3    `yaml:"kube-state-metrics"`
+	Rest             map[string]interface{} `yaml:",inline"`
 }
