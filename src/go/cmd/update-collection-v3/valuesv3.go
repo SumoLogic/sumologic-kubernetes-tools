1d8ebee0a8dd91326194db147bd3c2fac81c5ddc
Dominik Rosiek <drosiek@sumologic.com>
2022-07-29 16:40:10 +0200

    feat: do not save null fields
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>

--- src/go/cmd/update-collection-v3/valuesv3.go
+++ src/go/cmd/update-collection-v3/valuesv3.go
@@ -1,16 +1,16 @@
 package main
 
 type ValuesV3 struct {
-	KubePrometheusStack *KubePrometheusStackV3 `yaml:"kube-prometheus-stack"`
+	KubePrometheusStack *KubePrometheusStackV3 `yaml:"kube-prometheus-stack,omitempty"`
 	Rest                map[string]interface{} `yaml:",inline"`
 }
 
 type KubeStateMetricsV3 struct {
-	Collectors *[]string              `yaml:"collectors"`
+	Collectors *[]string              `yaml:"collectors,omitempty"`
 	Rest       map[string]interface{} `yaml:",inline"`
 }
 
 type KubePrometheusStackV3 struct {
-	KubeStateMetrics *KubeStateMetricsV3    `yaml:"kube-state-metrics"`
+	KubeStateMetrics *KubeStateMetricsV3    `yaml:"kube-state-metrics,omitempty"`
 	Rest             map[string]interface{} `yaml:",inline"`
 }
