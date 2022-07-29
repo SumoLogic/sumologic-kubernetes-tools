ad24bf97ff0275cdebcc336cc3c0d4f25fcd1591
Dominik Rosiek <drosiek@sumologic.com>
2022-07-29 16:40:35 +0200

    test(update-collection-v3): init tests for kube-state-metrics migration
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>

---
+++ src/go/cmd/update-collection-v3/migrate_test.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMigrateKubeStateMetricsCollectors(t *testing.T) {
+	for _, test := range []struct {
+		Name     string
+		V2Config *map[string]bool
+		V3Config *[]string
+	}{
+		{
+			Name:     "nil",
+			V2Config: nil,
+			V3Config: nil,
+		},
+		{
+			Name: "disable some collectors",
+			V2Config: &map[string]bool{
+				"certificatesigningrequests": false,
+				"deployments":                false,
+				"configmaps":                 true,
+				"poddisruptionbudgets":       false,
+			},
+			V3Config: &[]string{
+				"configmaps",
+				"cronjobs",
+				"daemonsets",
+				"endpoints",
+				"horizontalpodautoscalers",
+				"ingresses",
+				"jobs",
+				"limitranges",
+				"mutatingwebhookconfigurations",
+				"namespaces",
+				"networkpolicies",
+				"nodes",
+				"persistentvolumeclaims",
+				"persistentvolumes",
+				"pods",
+				"replicasets",
+				"replicationcontrollers",
+				"resourcequotas",
+				"secrets",
+				"services",
+				"statefulsets",
+				"storageclasses",
+				"validatingwebhookconfigurations",
+				"volumeattachments"},
+		},
+	} {
+		t.Run(test.Name, func(t *testing.T) {
+			result := migrateKubeStateMetricsCollectors(test.V2Config)
+			assert.Equal(t, test.V3Config, result)
+		})
+	}
+}
