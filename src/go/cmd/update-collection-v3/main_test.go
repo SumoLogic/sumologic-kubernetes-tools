a33ab133e7da772bd075073d631c295edc136df3
Dominik Rosiek <58699848+sumo-drosiek@users.noreply.github.com>
2022-08-12 10:53:05 +0200

    feat(update-collection-v3): add kube-state-metrics migration (#275)
    * feat(update-collection-v3): add kube-state-metrics migration
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    * feat: do not save null fields
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    * test(update-collection-v3): init tests for kube-state-metrics migration
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    * test(update-collection-v3/kube-state-metrics): add e2e test
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>

--- src/go/cmd/update-collection-v3/main_test.go
+++ src/go/cmd/update-collection-v3/main_test.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-	"testing"
 	"strings"
+	"testing"
+
 	"github.com/stretchr/testify/require"
 )
 
 type TestCase struct {
-	inputYaml string
-	outputYaml string
-	err error
+	inputYaml   string
+	outputYaml  string
+	err         error
 	description string
 }
 
@@ -21,4 +22,54 @@ func runYamlTest(t *testing.T, testCase TestCase) {
 		require.Equal(t, err, testCase.err, testCase.description)
 	}
 	require.Equal(t, strings.Trim(testCase.outputYaml, "\n "), strings.Trim(actualOutput, "\n "), testCase.description)
+}
+
+func TestYaml(t *testing.T) {
+	for _, tt := range []TestCase{
+		{
+			inputYaml: `
+kube-prometheus-stack:
+  kube-state-metrics:
+    collectors:
+      certificatesigningrequests: false
+      configmaps: true
+      persistentvolumes: false`,
+			outputYaml: `
+kube-prometheus-stack:
+    kube-state-metrics:
+        collectors:
+            - configmaps
+            - cronjobs
+            - daemonsets
+            - deployments
+            - endpoints
+            - horizontalpodautoscalers
+            - ingresses
+            - jobs
+            - limitranges
+            - mutatingwebhookconfigurations
+            - namespaces
+            - networkpolicies
+            - nodes
+            - persistentvolumeclaims
+            - poddisruptionbudgets
+            - pods
+            - replicasets
+            - replicationcontrollers
+            - resourcequotas
+            - secrets
+            - services
+            - statefulsets
+            - storageclasses
+            - validatingwebhookconfigurations
+            - volumeattachments
+`,
+			err:         nil,
+			description: "kube state metrics migration",
+		},
+	} {
+		t.Run(tt.description, func(t *testing.T) {
+			runYamlTest(t, tt)
+		})
+	}
 }
--- src/go/cmd/update-collection-v3/valuesv2.go
+++ src/go/cmd/update-collection-v3/valuesv2.go
@@ -1,5 +1,16 @@
 package main
 
 type ValuesV2 struct {
-	Rest map[string]interface{} `yaml:",inline"`
+	KubePrometheusStack *KubePrometheusStackV2 `yaml:"kube-prometheus-stack"`
+	Rest                map[string]interface{} `yaml:",inline"`
+}
+
+type KubeStateMetricsV2 struct {
+	Collectors *map[string]bool       `yaml:"collectors"`
+	Rest       map[string]interface{} `yaml:",inline"`
+}
+
+type KubePrometheusStackV2 struct {
+	KubeStateMetrics *KubeStateMetricsV2    `yaml:"kube-state-metrics"`
+	Rest             map[string]interface{} `yaml:",inline"`
 }
