a44ffca8966cdf7478865f2c9255aab94652c4fb
Andrzej Stencel <astencel@sumologic.com>
2022-10-17 09:49:51 +0200

    refactor(update-collection-v3): remove duplication

--- src/go/cmd/update-collection-v3/main_test.go
+++ src/go/cmd/update-collection-v3/main_test.go
@@ -12,23 +12,37 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestYamlFiles(t *testing.T) {
+func TestIndividualMigrations(t *testing.T) {
 	_, testFileName, _, _ := runtime.Caller(0)
-	testDir := path.Dir(testFileName)
-	inputFileNames, err := filepath.Glob(path.Join(testDir, "testdata", "*.input.yaml"))
+	currentTestDirectory := path.Dir(testFileName)
+	for directoryName, migrateFunction := range migrationDirectoriesAndFunctions {
+		testMigrationsInDirectory(t, migrateFunction, path.Join(currentTestDirectory, "migrations", directoryName, "testdata"))
+	}
+}
+
+func TestAllMigrations(t *testing.T) {
+	_, testFileName, _, _ := runtime.Caller(0)
+	currentTestDirectory := path.Dir(testFileName)
+	testMigrationsInDirectory(t, migrateYaml, path.Join(currentTestDirectory, "testdata"))
+}
+
+// testMigrationsInDirectory runs the migrate function on the *.input.yaml files
+// in the directory and asserts that the output matches the contents of the *.output.yaml files.
+func testMigrationsInDirectory(t *testing.T, migrate migrateFunc, directory string) {
+	inputFileNames, err := filepath.Glob(path.Join(directory, "*.input.yaml"))
 	require.NoError(t, err)
 	for _, inputFileName := range inputFileNames {
-		t.Run(path.Base(inputFileName), func(t *testing.T) {
-			outputFileName := strings.TrimSuffix(inputFileName, ".input.yaml")
-			outputFileName = outputFileName + ".output.yaml"
+		t.Run(path.Join(path.Base(path.Dir(directory)), path.Base(directory), path.Base(inputFileName)), func(t *testing.T) {
 			inputFileContents, err := ioutil.ReadFile(inputFileName)
 			require.NoError(t, err)
 
-			outputFileContents, err := migrateYaml(string(inputFileContents))
-
-			expectedOutputFileContents, err := ioutil.ReadFile(outputFileName)
+			actualOutput, err := migrate(string(inputFileContents))
 			require.NoError(t, err)
-			assert.Equal(t, string(expectedOutputFileContents), outputFileContents)
+
+			outputFileName := strings.TrimSuffix(inputFileName, ".input.yaml") + ".output.yaml"
+			expectedOutput, err := ioutil.ReadFile(outputFileName)
+			require.NoError(t, err)
+			assert.Equal(t, string(expectedOutput), actualOutput)
 		})
 	}
 }
