b30b510e7029418f2f951c02a820588899877385
Dominik Rosiek <drosiek@sumologic.com>
2022-10-19 11:33:18 +0200

    feat(update-collection-v3): run unit tests as well
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>

--- src/go/cmd/update-collection-v3/main.go
+++ src/go/cmd/update-collection-v3/main.go
@@ -8,7 +8,8 @@ import (
 	"os"
 
 	disablethanos "github.com/SumoLogic/sumologic-kubernetes-collection/tools/cmd/update-collection-v3/migrations/disable-thanos"
-	kubeprometheusstackandevents "github.com/SumoLogic/sumologic-kubernetes-collection/tools/cmd/update-collection-v3/migrations/kube-prometheus-stack-and-events"
+	"github.com/SumoLogic/sumologic-kubernetes-collection/tools/cmd/update-collection-v3/migrations/events"
+	kubeprometheusstack "github.com/SumoLogic/sumologic-kubernetes-collection/tools/cmd/update-collection-v3/migrations/kube-prometheus-stack"
 )
 
 var (
@@ -51,8 +52,9 @@ func migrateYamlFile(yamlV2FilePath string, yamlV3FilePath string) error {
 }
 
 var migrationDirectoriesAndFunctions = map[string]migrateFunc{
-	"kube-prometheus-stack-and-events": kubeprometheusstackandevents.Migrate,
-	"disable-thanos":                   disablethanos.Migrate,
+	"kube-prometheus-stack": kubeprometheusstack.Migrate,
+	"events":                events.Migrate,
+	"disable-thanos":        disablethanos.Migrate,
 }
 
 func migrateYaml(input string) (string, error) {
