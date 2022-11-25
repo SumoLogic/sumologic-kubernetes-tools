17408d9a961ae479b1632551865b77ba32a76ba0
Mikołaj Świątek <mswiatek@sumologic.com>
2022-11-25 09:03:09 +0100

    feat(update-collection-v3): migrate otelevents.config.override

---
+++ src/go/cmd/update-collection-v3/migrations/events-config-merge/migrate.go
@@ -0,0 +1,70 @@
+package eventsconfigmerge
+
+import (
+	"bytes"
+	"fmt"
+
+	"gopkg.in/yaml.v3"
+)
+
+type InputValues struct {
+	Otelevents struct {
+		Config struct {
+			Override map[string]interface{} `yaml:"override,omitempty"`
+		} `yaml:"config,omitempty"`
+		Rest map[string]interface{} `yaml:",inline"`
+	} `yaml:"otelevents,omitempty"`
+	Rest map[string]interface{} `yaml:",inline"`
+}
+
+type OutputValues struct {
+	Otelevents Otelevents             `yaml:"otelevents,omitempty"`
+	Rest       map[string]interface{} `yaml:",inline"`
+}
+
+type Otelevents struct {
+	Config Config                 `yaml:"config,omitempty"`
+	Rest   map[string]interface{} `yaml:",inline"`
+}
+
+type Config struct {
+	Merge    map[string]interface{} `yaml:"merge,omitempty"`
+	Override map[string]interface{} `yaml:"override,omitempty"`
+}
+
+func Migrate(inputYaml string) (outputYaml string, err error) {
+	inputValues, err := parseValues(inputYaml)
+	if err != nil {
+		return "", fmt.Errorf("error parsing input yaml: %v", err)
+	}
+
+	outputValues, err := migrate(&inputValues)
+	if err != nil {
+		return "", fmt.Errorf("error migrating: %v", err)
+	}
+
+	buffer := bytes.Buffer{}
+	encoder := yaml.NewEncoder(&buffer)
+	encoder.SetIndent(2)
+	err = encoder.Encode(outputValues)
+	return buffer.String(), err
+}
+
+func parseValues(inputYaml string) (InputValues, error) {
+	var inputValues InputValues
+	err := yaml.Unmarshal([]byte(inputYaml), &inputValues)
+	return inputValues, err
+}
+
+func migrate(inputValues *InputValues) (OutputValues, error) {
+	outputValues := OutputValues{
+		Rest: inputValues.Rest,
+		Otelevents: Otelevents{
+			Config: Config{
+				Merge: inputValues.Otelevents.Config.Override,
+			},
+			Rest: inputValues.Otelevents.Rest,
+		},
+	}
+	return outputValues, nil
+}
