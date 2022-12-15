e7df10371c27f79c8b0b12770c4ffcc5db192e86
Katarzyna Kujawa <kkujawa@sumologic.com>
2022-12-15 10:58:04 +0100

    chore(upgrade_to_v3): simplify code in fluentd logs config migration

--- src/go/cmd/update-collection-v3/migrations/fluentd-logs-configs/migrate.go
+++ src/go/cmd/update-collection-v3/migrations/fluentd-logs-configs/migrate.go
@@ -7,15 +7,15 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-func Migrate(yamlV2 string) (yamlV3 string, err error) {
-	valuesV2, err := parseValues(yamlV2)
+func Migrate(input string) (string, error) {
+	valuesV2, err := parseValues(input)
 	if err != nil {
 		return "", fmt.Errorf("error parsing input yaml: %v", err)
 	}
 
 	if valuesV2.Fluentd == nil || valuesV2.Fluentd.Logs == nil {
 		// migration of fluentd.logs keys is not needed
-		return yamlV2, nil
+		return input, nil
 	}
 
 	valuesV3 := migrate(&valuesV2)
@@ -27,16 +27,16 @@ func Migrate(yamlV2 string) (yamlV3 string, err error) {
 	return buffer.String(), err
 }
 
-func parseValues(yamlV2 string) (ValuesV2, error) {
+func parseValues(input string) (ValuesV2, error) {
 	var valuesV2 ValuesV2
-	err := yaml.Unmarshal([]byte(yamlV2), &valuesV2)
+	err := yaml.Unmarshal([]byte(input), &valuesV2)
 	return valuesV2, err
 }
 
 func migrate(valuesV2 *ValuesV2) ValuesV3 {
 	return ValuesV3{
 		Rest:      valuesV2.Rest,
-		Sumologic: setSumologic(valuesV2),
-		Fluentd:   setFluentdLogs(valuesV2),
+		Sumologic: createSumologic(valuesV2),
+		Fluentd:   createFluentdLogs(valuesV2),
 	}
 }
