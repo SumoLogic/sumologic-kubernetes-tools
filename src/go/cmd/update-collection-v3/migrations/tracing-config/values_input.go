bbf0b046083f1bd5eb32db07301fb2e4d05194c1
Mateusz "mat" Rumian <58699800+mat-rumian@users.noreply.github.com>
2023-01-27 14:33:09 +0100

    feat(tracing): improve migration (#449)

--- src/go/cmd/update-collection-v3/migrations/tracing-config/values_input.go
+++ src/go/cmd/update-collection-v3/migrations/tracing-config/values_input.go
@@ -2,19 +2,47 @@ package tracingconfig
 
 type ValuesInput struct {
 	Otelcol     Otelcol                `yaml:"otelcol,omitempty"`
+	Otelgateway Otelgateway            `yaml:"otelgateway,omitempty"`
 	Otelagent   map[string]interface{} `yaml:"otelagent,omitempty"`
-	Otelgateway map[string]interface{} `yaml:"otelgateway,omitempty"`
 	Rest        map[string]interface{} `yaml:",inline"`
 }
 
 type Otelcol struct {
-	Config struct {
+	Deployment map[string]interface{} `yaml:"deployment,omitempty"`
+	Config     struct {
 		Processors struct {
+			Batch           map[string]interface{} `yaml:"batch,omitempty"`
 			CascadingFilter map[string]interface{} `yaml:"cascading_filter,omitempty"`
+			MemoryLimiter   map[string]interface{} `yaml:"memory_lmiter,omitempty"`
 			Source          map[string]interface{} `yaml:"source,omitempty"`
 			Rest            map[string]interface{} `yaml:",inline"`
 		} `yaml:"processors,omitempty"`
 		Rest map[string]interface{} `yaml:",inline"`
+	} `yaml:"config,omitempty"`
+	Rest map[string]interface{} `yaml:",inline"`
+}
+
+type Otelgateway struct {
+	Deployment map[string]interface{} `yaml:"deployment,omitempty"`
+	Config     struct {
+		Exporters struct {
+			LoadBalancing struct {
+				Protocol struct {
+					Otlp struct {
+						Compression  string `yaml:"compression,omitempty"`
+						SendingQueue struct {
+							NumConsumers int `yaml:"num_consumers,omitempty"`
+							QueueSize    int `yaml:"queue_size,omitempty"`
+						} `yaml:"sending_queue,omitempty"`
+					} `yaml:"otlp,omitempty"`
+				} `yaml:"protocol,omitempty"`
+			} `yaml:"loadbalancing,omitempty"`
+			Rest map[string]interface{} `yaml:",inline"`
+		} `yaml:"exporters,omitempty"`
+		Processors struct {
+			Batch         map[string]interface{} `yaml:"batch,omitempty"`
+			MemoryLimiter map[string]interface{} `yaml:"memory_lmiter,omitempty"`
+		} `yaml:"processors,omitempty"`
 	} `yaml:"config,omitempty"`
 	Rest map[string]interface{} `yaml:",inline"`
 }
