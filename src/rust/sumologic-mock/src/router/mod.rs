32980e4b59e9be3f78375b91aa1a1404306a3cf6
Jaganathan P <156800415+jagan2221@users.noreply.github.com>
2026-06-08 13:06:47 +0530

    Update mod.rs

--- src/rust/sumologic-mock/src/router/mod.rs
+++ src/rust/sumologic-mock/src/router/mod.rs
@@ -281,7 +281,7 @@ pub async fn handler_receiver(
 
 fn try_dropping_data(opts: &web::Data<options::Options>, content_type: &str) -> Option<HttpResponse> {
     let mut rng = rand::thread_rng();
-    let number: i64 = rng.gen_range(0..100);
+    let number: i64 = rng.random_range(0..100);
     if number < opts.drop_rate {
         let msg = format!("Dropping data for {}", content_type);
         debug!("{}", msg);
